d1cf1d2d5d9bff0506e2fb912bfa6d6b453bd97e
Shra1497 <126476795+Shra1497@users.noreply.github.com>
2023-02-28 16:37:47 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -2,7 +2,7 @@
     include 'db.php';
     session_start();
     if(!(isset($_SESSION['email']))){
-        header("location:login.php");
+        header("location:index.php");
     }else{
         $name = $_SESSION['name'];
         $email = $_SESSION['email'];
